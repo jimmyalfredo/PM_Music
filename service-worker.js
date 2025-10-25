0f535094a36fac72a138729f23d76479821676e3
jimmyalfredo <jimmyalfred@gmail.com>
2025-10-24 20:30:43 -0600

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -9,3 +9,4 @@ const urlsToCache = [
 self.addEventListener('install', e=>e.waitUntil(caches.open(CACHE_NAME).then(cache=>cache.addAll(urlsToCache))));
 self.addEventListener('fetch', e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
 
+
